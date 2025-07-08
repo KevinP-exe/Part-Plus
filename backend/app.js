1399ede0ce55e747c927f995ec685a092eb796bf
kevshito <20230002@ricaldone.edu.sv>
2025-07-08 11:19:30 -0600

    v3

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express"
 import clientsRoute from "./src/routes/clientsRoute.js"
 import reservationsRoute from "./src/routes/reservationsRoute.js"
 
@@ -6,6 +7,11 @@ const app = express();
 
 app.use(express.json());
 
+const swaggerDocument = JSON parse(
+    fs.readFileSync(path.resolve("./kevshito-PartPlus-1.0.0-resolved.json"), "utf-8")
+); 
+
+app.use ("api/docs".swaggerUi.serve.swaggerUi.setup(swaggerDocument));
 app.use("/api/clients", clientsRoute);
 app.use("/api/reservations", reservationsRoute);
 
